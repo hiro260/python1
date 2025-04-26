42f8844b8fb64790cd468ac25d833d26262eee5b
hiro260 <hsanda777@gmail.com>
2025-04-26 11:56:31 -0600

    update

---
+++ oop/new1.py
@@ -0,0 +1,34 @@
+fruits = ['apple', 'peach', 'grapes', 'banana']
+
+# for fruit in fruits:
+#     print(fruit)
+# else:
+#     print("loop is done")
+
+
+# for idx, fruit in enumerate(fruits):
+#     print(idx)
+#     print(fruit)
+
+
+# for x in enumerate(fruits):
+#     print(x)
+
+# square_list = []
+# for i in range(10):
+#     square_list.append(i ** 2)
+
+# print(square_list)
+
+# print([i ** 2 for i in range(10)])
+# print([i ** 2 for i in range(10) if i % 2 == 0])
+
+data_of_birth = (1990, 2, 3)
+#data_of_birth[0] =1999
+
+print(data_of_birth)
+
+year, month, date = data_of_birth
+print(year)
+print(month)
+print(date)
