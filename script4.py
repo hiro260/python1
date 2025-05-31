5cde3f34087fd491d4dff73ec9d912f26388bef1
hiro260 <hsanda777@gmail.com>
2025-05-31 17:22:08 -0600

    update

--- script4.py
+++ script4.py
@@ -1,3 +1,18 @@
+import time
+from re import search
+
+print(dir())
+
+
+
+
+
+
+
+
+quit()
+
+
 def my_decorator(func):
     def wrapper(*args, **kwargs):
         print("Something is happening before the function is called.")
@@ -13,3 +28,28 @@ def say_hello(name):
 
 say_hello("Alice")
 
+import sys
+
+print(sys.path)
+import numpy as np
+print("\n")
+print(np.__file__)
+
+print("\n")
+
+import time
+print(time.time()/(60*60*24*365))
+print(time.ctime())
+localtime = time.localtime()
+print(localtime)
+print(localtime.tm_year)
+print(localtime.tm_mon)
+print(localtime.tm_mday)
+print(localtime.tm_hour)
+print(localtime.tm_min)
+
+sec = 10
+#time.sleep(sec)
+
+
+print(dir(__builtins__))
