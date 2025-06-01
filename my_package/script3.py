2bfb3017c918897a167addba63819fb5a11fd8c8
hiro260 <hsanda777@gmail.com>
2025-05-31 18:01:17 -0600

    sacipt3

--- my_package/script3.py
+++ my_package/script3.py
@@ -1,23 +1,13 @@
 def function4(w, x, y, z):
     return w - x - y - z
 
+
 def function5(w, x):
     return w // x
 
+
 def function6(w, x):
     return w % x
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
