ce2de453d15e7a84223c46c1f901816e95c4a394
hiro260 <hsanda777@gmail.com>
2025-05-31 18:04:21 -0600

    new3.py

--- test2/new3.py
+++ test2/new3.py
@@ -1,6 +1,7 @@
 x, y, z = (1, 2, 3)
 print(x)
 
+
 def get_average(*args):
     print(args)
     num = len(args)
@@ -9,15 +10,16 @@ def get_average(*args):
     total = sum(args)
     return total / num
 
+
 average = get_average(1, 2, 3, 4, 5)
 print(average)
 
 
 def kwargs_func(**kwargs):
-    param1 = kwargs.get('param1', 1)
-    param2 = kwargs.get('param2', 1)
-    param3 = kwargs.get('param3', 1)
+    param1 = kwargs.get("param1", 1)
+    param2 = kwargs.get("param2", 1)
+    param3 = kwargs.get("param3", 1)
     print(f"param1: {param1}, param2: {param2}, param3: {param3}")
 
-kwargs_func(param1=10, param2=6, param3=100)
 
+kwargs_func(param1=10, param2=6, param3=100)
